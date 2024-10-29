e3ad4dc9b00d02b700281f9c0581039409b304c9
axtk <axtkx@inbox.ru>
2024-10-29 16:32:59 +0300

    use head style in default renderStatus

--- src/utils/renderStatus.ts
+++ src/utils/renderStatus.ts
@@ -1,8 +1,8 @@
 import {STATUS_CODES} from 'node:http';
 import {RenderStatus} from '../types/RenderStatus';
 
-export const renderStatus: RenderStatus = (req, {statusCode}) => {
-    let id = req.ctx?.id;
+export const renderStatus: RenderStatus = ({ctx = {}}, {statusCode}) => {
+    let {id, nonce} = ctx;
     let statusText = `${statusCode} ${STATUS_CODES[statusCode]}`;
     let date = (new Date())
         .toISOString()
@@ -12,9 +12,10 @@ export const renderStatus: RenderStatus = (req, {statusCode}) => {
     return '<!DOCTYPE html>' +
         '<html><head><meta charset="utf-8"/>' +
         '<meta name="viewport" content="width=device-width"/>' +
-        `<title>${statusText}</title></head>` +
-        '<body><main style="text-align: center;">' +
-        `<h1>${statusText}</h1><hr/><p>` +
+        `<title>${statusText}</title>` +
+        `<style${nonce ? ` nonce="${nonce}"` : ''}>` +
+        'main{text-align:center;}</style>' +
+        `<body><main><h1>${statusText}</h1><hr/><p>` +
         (id ? `<code>ID: ${id}</code><br/>` : '') +
         `<code>${date}</code>` +
         '</p></main></body></html>';
